497a99653839518fca4e2e2060e3923110e0d3c1
Myles Marshall <mmarsh319@west-mec.org>
2025-03-11 16:43:23 -0700

    second overlayed canvas added

--- canvas.js
+++ canvas.js
@@ -1,10 +1,18 @@
 const background = document.getElementById("backgroundCanvas");
-const ctx = background.getContext("2d");
+const character = document.getElementById("characterCanvas");
+
+const bgCtx = background.getContext("2d");
+const charCtx = character.getContext("2d");
 
 document.addEventListener("resize", () => {
     background.height = window.innerHeight;
     background.width = window.innerWidth;
+    character.height = window.innerHeight;
+    character.width = window.innerWidth;
 });
 
-ctx.fillStyle = "black";
-ctx.fillRect(10, 10, 100, 100);
+bgCtx.fillStyle = "black";
+bgCtx.fillRect(10, 10, 100, 100);
+
+charCtx.fillStyle = "green";
+charCtx.fillRect(20, 20, 100, 100);
