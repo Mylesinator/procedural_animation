4d0cc14acb6834e87f3ff2f8c12ec3be03d2fb63
Myles Marshall <mmarsh319@west-mec.org>
2025-03-11 17:20:34 -0700

    asdasd

--- canvas.js
+++ canvas.js
@@ -4,15 +4,23 @@ const character = document.getElementById("characterCanvas");
 const bgCtx = background.getContext("2d");
 const charCtx = character.getContext("2d");
 
-document.addEventListener("resize", () => {
+function setCanvasSize() {
     background.height = window.innerHeight;
     background.width = window.innerWidth;
     character.height = window.innerHeight;
     character.width = window.innerWidth;
-});
 
-bgCtx.fillStyle = "black";
-bgCtx.fillRect(10, 10, 100, 100);
+    draw();
+}
 
-charCtx.fillStyle = "green";
-charCtx.fillRect(20, 20, 100, 100);
+function draw() {
+    bgCtx.fillStyle = "black";
+    bgCtx.fillRect(10, 10, 100, 100);
+
+    charCtx.fillStyle = "green";
+    charCtx.fillRect(20, 20, 100, 100);
+}
+
+window.addEventListener("resize", setCanvasSize);
+setCanvasSize();
+draw();
